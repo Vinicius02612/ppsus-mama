f71da842fb60331bbb2070aae82c9671a101615e
Vinicius02612 <viniciusnunes02612@gmail.com>
2023-04-29 15:01:47 -0300

    Mudando porta do host

--- ppsus_mama/account/urls.py
+++ ppsus_mama/account/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [ 
-    path('', views.login, name='index_login'),
     path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('dashboard/', views.dashboard, name='dashboard'),
