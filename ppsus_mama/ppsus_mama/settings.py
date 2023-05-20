9a06bb6e5ed00ee7babef3e2a7c10ef64a86e38c
Vinicius02612 <viniciusnunes02612@gmail.com>
2023-05-20 17:43:12 -0300

    resolvendo erro de render

--- ppsus_mama/ppsus_mama/settings.py
+++ ppsus_mama/ppsus_mama/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wj=!2)n*x9!dfq*8xzt*8njb2i7435=*ic(@h04#g$e$xb=lq5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['3.131.160.163']
 
 
 # Application definition
