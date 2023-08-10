77e3113ab5f2ce016d86448b0f13aba87c71b1b2
Vinicius02612 <viniciusnunes02612@gmail.com>
2023-08-09 22:36:01 -0300

    alter settings.py

--- ppsus_mama/settings.py
+++ ppsus_mama/settings.py
@@ -26,9 +26,9 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wj=!2)n*x9!dfq*8xzt*8njb2i7435=*ic(@h04#g$e$xb=lq5'
+""" SECRET_KEY = 'django-insecure-wj=!2)n*x9!dfq*8xzt*8njb2i7435=*ic(@h04#g$e$xb=lq5' """
 
-""" SECRET_KEY = os.getenv('SECRET_KEY') """
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
