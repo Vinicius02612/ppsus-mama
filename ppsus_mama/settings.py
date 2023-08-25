32e729c0ca0e1aa16ef3a75f810f6fb52408a9ac
Vinicius02612 <viniciusnunes02612@gmail.com>
2023-08-24 23:21:27 -0300

    corrigindo erro de coluna.

--- ppsus_mama/settings.py
+++ ppsus_mama/settings.py
@@ -26,21 +26,21 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wj=!2)n*x9!dfq*8xzt*8njb2i7435=*ic(@h04#g$e$xb=lq5'
+""" SECRET_KEY = 'django-insecure-wj=!2)n*x9!dfq*8xzt*8njb2i7435=*ic(@h04#g$e$xb=lq5' """
 
-""" SECRET_KEY = os.getenv('SECRET_KEY') """
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 """ DEBUG = False """
-DEBUG = True
+""" DEBUG = True """
 
-""" DEBUG = os.getenv('DEBUG','0').lower() in ['true','t','1'] """
+DEBUG = os.getenv('DEBUG','0').lower() in ['true','t','1']
 
 """ 127.0.0.1 """
-ALLOWED_HOSTS = ['127.0.0.1']
+""" ALLOWED_HOSTS = ['127.0.0.1'] """
 
-""" ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ') """
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 
@@ -98,10 +98,7 @@ WSGI_APPLICATION = 'ppsus_mama.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
     
 """
