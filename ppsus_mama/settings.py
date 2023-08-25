b0c61f19779f9fc39225bca060786b4fca7b2857
Vinicius02612 <viniciusnunes02612@gmail.com>
2023-08-24 23:18:04 -0300

    corrigindo erro de coluna.

--- ppsus_mama/settings.py
+++ ppsus_mama/settings.py
@@ -33,14 +33,14 @@ SECRET_KEY = 'django-insecure-wj=!2)n*x9!dfq*8xzt*8njb2i7435=*ic(@h04#g$e$xb=lq5
 # SECURITY WARNING: don't run with debug turned on in production!
 
 """ DEBUG = False """
-""" DEBUG = True """
+DEBUG = True
 
-DEBUG = os.getenv('DEBUG','0').lower() in ['true','t','1']
+""" DEBUG = os.getenv('DEBUG','0').lower() in ['true','t','1'] """
 
 """ 127.0.0.1 """
-""" ALLOWED_HOSTS = ['127.0.0.1'] """
+ALLOWED_HOSTS = ['127.0.0.1']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+""" ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ') """
 
 # Application definition
 
@@ -98,7 +98,10 @@ WSGI_APPLICATION = 'ppsus_mama.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
     
 """
